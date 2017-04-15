d0d2c9c6c8a172e37d94935e42ef5f74cf873117
Matt Thompson <mattt@defunct.ca>
2017-04-15 16:09:51 -0400

    Update script to use PouchDB
    This update modifies the script to use PouchDB for local storage,
    instead of a JSON file.

--- todo.js
+++ todo.js
@@ -1,158 +1,78 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
+var PouchDB = require('pouchdb-node');
 var program = require('commander');
 
-function ToDo(desc) {
-  this.description = desc;
-  this.created_at = Date.now();
-  this.status = "pending";
-}
+var db = new PouchDB('todo');
+var remoteCouch = false;
 
-function add(list, desc) {
-  var todo = new ToDo(desc);
-
-  list.push(todo);
-  write(list);
-  console.log('The following todo has been added:');
-  console.log(todo);
-}
-
-function edit(list, timestamp, desc) {
-  var i;
-      found = null;
-
-  for (i = 0; i < list.length; i++) {
-    if (list[i] !== null && list[i]['created_at'] === parseInt(timestamp)) {
-      list[i].description = desc;
-      found = list[i];
-      /*
-        We break here so we can exit without inspecting any further entries,
-        while also being able to use the current values of list and i below.
-      */ 
-      break;
-    }
-  }
-
-  if (found !== null) {
-    write(list);
-    console.log('The following todo has been updated:');
-    console.log(found);
-  }
-}
-
-function complete(list, timestamp) {
-  var i;
-      found = null;
-  
-  for (i = 0; i < list.length; i++) {
-    if (list[i] !== null && list[i]['created_at'] === parseInt(timestamp)) {
-      found = list[i];
-      list[i].status = "done";
-      /*
-        We break here so we can exit without inspecting any further entries,
-        while also being able to use the current values of list and i below.
-      */ 
-      break;
-    }
-  }
-
-  if (found !== null) {
-    write(list);
-    console.log('The following todo has been completed:');
-    console.log(found);
-  }
-}
-
-function rm(list, timestamp) {
-  var i;
-      found = null;
-  
-  for (i = 0; i < list.length; i++) {
-    if (list[i] !== null && list[i]['created_at'] === parseInt(timestamp)) {
-      found = list[i];
-      list[i].status = "deleted";
-      /*
-        We break here so we can exit without inspecting any further entries,
-        while also being able to use the current values of list and i below.
-      */ 
-      break;
-    }
-  }
-
-  if (found !== null) {
-    write(list);
-    console.log('The following todo has been deleted:');
-    console.log(found);
-  }
-}
-
-function display(list) {
-  var i;
-
-  console.log('Timestamp\tDone\tDescription');
-
-  for (i = 0; i < list.length; i++) {
-    // TODO: Add ability to show completed todos
-    //if (list[i] !== null && list[i].done === false) {
-    if (list[i] !== null) {
-      console.log(list[i].created_at + '\t' + list[i].status + '\t' + list[i].description);
-    }
+function add(desc) {
+  var todo = {
+    _id: Date.now().toString(),
+    description: desc,
+    status: 'pending'
   };
-}
 
-function read(callback, filter = 'all') {
-  var i,
-      list,
-      args = [];
-
-  for (i = 1; i < arguments.length; i++) {
-    args.push(arguments[i]);
-  }
-
-  fs.readFile('todo.json', 'utf8', function (err, data) {
-    // file does not exist
-    if (err && err.code === 'ENOENT') {
-      write([]);
+  db.put(todo, function callback(err, result) {
+    if (!err) {
+      console.log('The following todo has been added:');
+      console.log(todo);
     }
-
-    // undefined when file is empty
-    if (data === undefined) {
-      list = [];
-    } else {
-      list = JSON.parse(data);
-    }
-
-    for (i = 0; i < list.length; i++) {
-      if (list[i] === null) {
-        continue;
-      }
-
-      if (filter === 'done' && list[i].status != 'done') {
-        /*
-          Deleting the entry doesn't seem to mark it as null, which happens
-          when you delete an entry and then write the file.
-        */
-        //delete list[i]
-        list[i] = null;
-      } else if (filter === 'pending' && list[i].status != 'pending') {
-        list[i] = null;
-      }
-    }
-
-    /*
-     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_operator
-     "Spread syntax" (...args) takes the array and expands it in place to be
-     used as function arguments. Simply passing args would pass an array, which
-     would need to be separated on the receiving end
-    */
-    callback(list, ...args);
   });
 }
 
-function write(list) {
-  fs.writeFile('todo.json', JSON.stringify(list, null, '  '), 'utf-8', function (err) {
-    // TODO: Add some error handling here
+function edit(timestamp, desc) {
+  db.get(timestamp, function(err, doc) {
+    db.put({
+      _id: timestamp,
+      _rev: doc._rev,
+      status: doc.status,
+      description: desc
+    }, function(err, response) {
+      if (err) { return console.log(err); }
+      console.log('The following todo has been updated:');
+      console.log(doc);
+    });
+  });
+}
+
+function complete(timestamp) {
+  db.get(timestamp, function(err, doc) {
+    db.put({
+      _id: timestamp,
+      _rev: doc._rev,
+      status: 'done',
+      description: doc.description
+    }, function(err, response) {
+      if (err) { return console.log(err); }
+      console.log('The following todo has been completed:');
+      console.log(doc);
+    });
+  });
+}
+
+function rm(timestamp) {
+  db.get(timestamp, function(err, doc) {
+    if (err) { return console.log(err); }
+    db.remove(doc, function(err, response) {
+      if (err) { return console.log(err); }
+      console.log('The following todo has been deleted:');
+      console.log(doc);
+    });
+  })
+}
+
+function list(filter = 'all') {
+  var i;
+
+  console.log('Timestamp\tStatus\tDescription');
+
+  db.allDocs({include_docs: true, descending: false}, function (err, doc) {
+    for (i = 0; i < doc.rows.length; i++) {
+      if (filter === 'all' || filter === doc.rows[i].doc.status) {
+        console.log(doc.rows[i].doc._id + '\t' + doc.rows[i].doc.status + '\t' + doc.rows[i].doc.description);
+      }
+    }
   });
 }
 
@@ -172,39 +92,40 @@ program
   .description('list todos')
   .option("-f, --filter [status]", "...")
   .action(function(options){
-    if (options.filter === "done" || options.filter === "all") {
-      read(display, options.filter);
-    } else {
-      read(display, "pending");
+    if (options.filter === undefined) {
+      options.filter = 'pending';
     }
+
+    list(options.filter);
   });
 
 program
   .command('add <desc>')
   .description('add todo')
   .action(function(desc, options){
-    read(add, desc);
+    add(desc);
   });
 
 program
   .command('edit <timestamp> <desc>')
   .description('edit todo')
   .action(function(timestamp, desc, options){
-    read(edit, timestamp, desc);
+    edit(timestamp, desc);
   });
 
 program
   .command('complete <timestamp>')
   .description('complete todo')
   .action(function(timestamp, options){
-    read(complete, timestamp);
+    complete(timestamp);
   });
 
 program
   .command('rm <timestamp>')
   .description('delete todo')
   .action(function(timestamp, options){
-    read(rm, timestamp);
+    rm(timestamp);
   });
+
 
 program.parse(process.argv);
